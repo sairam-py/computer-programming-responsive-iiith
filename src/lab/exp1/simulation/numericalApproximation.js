b81d03449a4af891e1c766aa52c54b1eb2640cc1
sairam-py <sairam.kollimarla@yahoo.com>
2020-07-07 21:41:18 +0530

    CSE1 set limit to 10

--- src/lab/exp1/simulation/numericalApproximation.js
+++ src/lab/exp1/simulation/numericalApproximation.js
@@ -10,6 +10,7 @@ window.model = {
 	sum: 0, //total sum that compute by computeSum method. 
 	width: 1, //width of executing one step.
 	//  computeSum: compute total sum of area under cos curve.
+
 	computeSum: function () {
     	this.sum = this.sum + Math.cos(2 * Math.PI/13 * this.inputValueA) * this.width;
     	},
@@ -290,8 +291,8 @@ window.view = {
 			alert('Enter numeric value of a and b');
 			return false;
 		} 
-		else if (valueA2 >= valueB2 || valueB2 > 30) {
-			alert('Integration Limits are from 0 to 30, b > a and b-a >= 1');
+		else if (valueA2 >= valueB2 || valueB2 > 10) {
+			alert('Integration Limits are from 0 to 10, b > a and b-a >= 1');
 			return false;
 		}
 		else {
